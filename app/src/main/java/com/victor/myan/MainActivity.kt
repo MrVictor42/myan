4035084448f1eee431300b4e4f35f9ae2561866c
MrVictor42 <guin-hu@hotmail.com>
2021-04-24 12:02:25 -0300

    Insert images of luffy and gohan to slides. Create slides pages and change color status bar of main activity

--- app/src/main/java/com/victor/myan/MainActivity.kt
+++ app/src/main/java/com/victor/myan/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+        val window = this.window
+        window.statusBarColor = this.resources.getColor(R.color.orange)
         Handler(Looper.getMainLooper()).postDelayed({
             val slidesIntent = Intent(this, Slides::class.java)
             startActivity(slidesIntent)
--- app/src/main/java/com/victor/myan/Slides.kt
+++ app/src/main/java/com/victor/myan/Slides.kt
@@ -3,13 +3,36 @@ package com.victor.myan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
 
 class Slides : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_slides)
 
         isButtonBackVisible = false
         isButtonNextVisible = false
+
+        addSlide(
+            SimpleSlide.Builder()
+                    .background(R.color.yellow)
+                    .image(R.drawable.gohan)
+                    .backgroundDark(R.color.yellow)
+                    .title("Everything In Same Place!")
+                    .description("Follow The Animes of The Season, Search For Animes " +
+                            "From Previous Seasons, All in One Place!!")
+                    .build()
+        )
+
+        addSlide(
+                SimpleSlide.Builder()
+                        .background(R.color.red)
+                        .image(R.drawable.luffy)
+                        .backgroundDark(R.color.red)
+                        .title("Every Week, One New Story!")
+                        .canGoBackward(true)
+                        .description("Follow The Weekly Animes, Discover New Stories " +
+                                "And Paths!")
+                        .build()
+        )
     }
 }
