f1cc8e0590de749a3a45f897be38af06d783f733
MrVictor42 <guin-hu@hotmail.com>
2021-05-10 23:53:44 -0300

    Context in adapter and decreasing height of imageview in list_adapter.xml

--- app/src/main/java/com/victor/myan/adapter/TodayAnimeAdapter.kt
+++ app/src/main/java/com/victor/myan/adapter/TodayAnimeAdapter.kt
@@ -1,11 +1,10 @@
 package com.victor.myan.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import com.victor.myan.R
@@ -15,9 +14,13 @@ class TodayAnimeAdapter(var items: MutableList<Anime>) : RecyclerView.Adapter<To
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageurl = view.findViewById<ImageView>(R.id.image_url)
-        fun bind(anime: Anime) {
+        fun bind(anime: Anime, holder: MyViewHolder) {
             val imageUrl = anime.image_url
             Picasso.get().load(imageUrl).into(imageurl)
+
+            holder.itemView.setOnClickListener {
+                Toast.makeText(holder.itemView.context, anime.title, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -27,11 +30,7 @@ class TodayAnimeAdapter(var items: MutableList<Anime>) : RecyclerView.Adapter<To
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(items[position])
-        val item = items[position]
-        holder.itemView.setOnClickListener {
-            Log.e("Clicado: ", item.toString())
-        }
+        holder.bind(items[position], holder)
     }
 
     override fun getItemCount(): Int {
