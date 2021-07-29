49bada2f8570080f157cb6c76793b74871750396
MrVictor42 <guin-hu@hotmail.com>
2021-07-28 23:30:23 -0300

    Inicial example carousel

--- app/src/main/java/com/victor/myan/fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/victor/myan/fragments/ViewPagerFragment.kt
@@ -1,24 +1,16 @@
 package com.victor.myan.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.viewpager.widget.ViewPager
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
 import com.victor.myan.R
 import com.victor.myan.adapter.ViewPagerAnimeSlideAdapter
-import com.victor.myan.api.CategoryApi
 import com.victor.myan.databinding.FragmentViewPagerBinding
-import com.victor.myan.helper.JikanApiInstanceHelper
-import com.victor.myan.model.Anime
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class ViewPagerFragment : Fragment() {
 
@@ -34,94 +26,113 @@ class ViewPagerFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val viewPagerAnime = binding.viewPagerAnime
-        val animeList = arrayListOf<Anime>()
-        viewPagerAnimeSlideAdapter = ViewPagerAnimeSlideAdapter(animeList)
-        viewPagerAnime.adapter = viewPagerAnimeSlideAdapter
+        val carouselView = binding.carouselView
+        var sampleImages = intArrayOf(
+            R.drawable.gohan,
+            R.drawable.luffy,
+        )
 
-        viewPagerAnime.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-                addDots(view, position)
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
-        })
-
-        val api = JikanApiInstanceHelper.getJikanApiInstance().create(CategoryApi::class.java)
-        api.slide("airing","score", 12).enqueue(object :
-            Callback<JsonObject> {
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-
-            }
-
-            override fun onResponse(
-                call: Call<JsonObject>,
-                response: Response<JsonObject>
-            ) {
-                if (response.isSuccessful) {
-                    val animeResponse = response.body()
-                    viewPagerAnimeSlideAdapter.anime.clear()
-                    if (animeResponse != null) {
-                        val results: JsonArray? =
-                            animeResponse.getAsJsonArray("results")
-                        if (results != null) {
-                            for (result in 0 until results.size()) {
-                                val animeFound: JsonObject? =
-                                    results.get(result) as JsonObject?
-                                if (animeFound != null) {
-                                    val anime = Anime()
-
-                                    anime.title = animeFound.get("title").asString
-                                    anime.mal_id =
-                                        animeFound.get("mal_id").asInt.toString()
-                                    anime.episodes = animeFound.get("episodes").asInt
-                                    anime.image_url =
-                                        animeFound.get("image_url").asString
-                                    anime.score = animeFound.get("score").asDouble
-                                    anime.synopsis = animeFound.get("synopsis").asString
-
-                                    if(animeFound.get("start_date").toString() == "null") {
-                                        anime.airing_start = ""
-                                    } else {
-                                        anime.airing_start = animeFound.get("start_date").asString
-                                    }
-
-                                    viewPagerAnimeSlideAdapter.anime.add(anime)
-                                }
-                            }
-                            viewPagerAnimeSlideAdapter.notifyDataSetChanged()
-                            addDots(view)
-                        }
-                    }
-                }
-            }
-        })
-    }
-
-    private fun addDots(view: View, position : Int = 0) {
-        val dotsLinear = binding.dots
-        dotsLinear.removeAllViews()
-        Array(viewPagerAnimeSlideAdapter.anime.size) {
-            val textView = TextView(view.context).apply {
-                text = "•"
-                textSize = 35f
-                if(position == it) {
-                    setTextColor(ContextCompat.getColor(view.context, R.color.white))
-                } else {
-                    setTextColor(ContextCompat.getColor(view.context, R.color.gray))
-                }
-            }
-            dotsLinear.addView(textView)
+        carouselView.pageCount = sampleImages.size
+        carouselView.setImageListener { position, imageView -> // You can use Glide or Picasso here
+            Picasso.get().load(sampleImages[position]).into(imageView)
         }
+
+        carouselView.setImageClickListener { position ->
+            Toast.makeText(
+                context,
+                "Clicked item: $position",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+//        val viewPagerAnime = binding.viewPagerAnime
+//        val animeList = arrayListOf<Anime>()
+//        viewPagerAnimeSlideAdapter = ViewPagerAnimeSlideAdapter(animeList)
+//        viewPagerAnime.adapter = viewPagerAnimeSlideAdapter
+//
+//        viewPagerAnime.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+//            override fun onPageScrolled(
+//                position: Int,
+//                positionOffset: Float,
+//                positionOffsetPixels: Int
+//            ) {
+//
+//            }
+//
+//            override fun onPageSelected(position: Int) {
+//                addDots(view, position)
+//            }
+//
+//            override fun onPageScrollStateChanged(state: Int) {
+//
+//            }
+//        })
+//
+//        val api = JikanApiInstanceHelper.getJikanApiInstance().create(CategoryApi::class.java)
+//        api.slide("airing","score", 12).enqueue(object :
+//            Callback<JsonObject> {
+//            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+//
+//            }
+//
+//            override fun onResponse(
+//                call: Call<JsonObject>,
+//                response: Response<JsonObject>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val animeResponse = response.body()
+//                    viewPagerAnimeSlideAdapter.anime.clear()
+//                    if (animeResponse != null) {
+//                        val results: JsonArray? =
+//                            animeResponse.getAsJsonArray("results")
+//                        if (results != null) {
+//                            for (result in 0 until results.size()) {
+//                                val animeFound: JsonObject? =
+//                                    results.get(result) as JsonObject?
+//                                if (animeFound != null) {
+//                                    val anime = Anime()
+//
+//                                    anime.title = animeFound.get("title").asString
+//                                    anime.mal_id =
+//                                        animeFound.get("mal_id").asInt.toString()
+//                                    anime.episodes = animeFound.get("episodes").asInt
+//                                    anime.image_url =
+//                                        animeFound.get("image_url").asString
+//                                    anime.score = animeFound.get("score").asDouble
+//                                    anime.synopsis = animeFound.get("synopsis").asString
+//
+//                                    if(animeFound.get("start_date").toString() == "null") {
+//                                        anime.airing_start = ""
+//                                    } else {
+//                                        anime.airing_start = animeFound.get("start_date").asString
+//                                    }
+//
+//                                    viewPagerAnimeSlideAdapter.anime.add(anime)
+//                                }
+//                            }
+//                            viewPagerAnimeSlideAdapter.notifyDataSetChanged()
+//                            addDots(view)
+//                        }
+//                    }
+//                }
+//            }
+//        })
+//    }
+//
+//    private fun addDots(view: View, position : Int = 0) {
+//        val dotsLinear = binding.dots
+//        dotsLinear.removeAllViews()
+//        Array(viewPagerAnimeSlideAdapter.anime.size) {
+//            val textView = TextView(view.context).apply {
+//                text = "•"
+//                textSize = 35f
+//                if(position == it) {
+//                    setTextColor(ContextCompat.getColor(view.context, R.color.white))
+//                } else {
+//                    setTextColor(ContextCompat.getColor(view.context, R.color.gray))
+//                }
+//            }
+//            dotsLinear.addView(textView)
+//        }
+//    }
     }
 }
