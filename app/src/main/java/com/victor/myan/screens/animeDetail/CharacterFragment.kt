065029d30997f51bc3556c431995b6afcbf7603d
MrVictor42 <guin-hu@hotmail.com>
2021-08-18 09:19:07 -0300

    Loadmore character

--- app/src/main/java/com/victor/myan/screens/animeDetail/CharacterFragment.kt
+++ app/src/main/java/com/victor/myan/screens/animeDetail/CharacterFragment.kt
@@ -17,11 +17,23 @@ import com.victor.myan.model.Character
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.view.ViewTreeObserver.OnScrollChangedListener
+import android.widget.AbsListView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.victor.myan.api.AnimeApi
+
 
 class CharacterFragment : Fragment() {
 
     private lateinit var binding : FragmentCharacterBinding
     private lateinit var characterAdapter : CharactersAdapter
+    private lateinit var manager : GridLayoutManager
+    private var isScrolling = false
+    private var currentItems = 0
+    private var totalItems = 0
+    private var scrollOutItems = 0
 
     companion object {
         fun newInstance(mal_id : String): CharacterFragment {
@@ -43,13 +55,77 @@ class CharacterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val characterRecyclerView = binding.animeCharacter
+        val progressBar = binding.progressBar
         val characterList = arrayListOf<Character>()
-        val malID = arguments?.getString("mal_id").toString()
-        val characterApi = JikanApiInstanceHelper.getJikanApiInstance().create(CharacterApi::class.java)
 
-        characterRecyclerView.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        manager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        characterRecyclerView.layoutManager = manager
         characterAdapter = CharactersAdapter(characterList)
         characterRecyclerView.adapter = characterAdapter
+        characterRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                currentItems = (characterRecyclerView.layoutManager as LinearLayoutManager).childCount
+                totalItems = (characterRecyclerView.layoutManager as LinearLayoutManager).itemCount
+                scrollOutItems = (characterRecyclerView.layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition()
+
+                if(isScrolling && (currentItems + scrollOutItems == totalItems)) {
+                    isScrolling = false
+                    loadMore()
+                }
+            }
+        })
+        loadMore()
+
+//
+//        characterApi.getCharactersStaff(malID).enqueue(object : Callback<JsonObject> {
+//            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+//
+//            }
+//
+//            @SuppressLint("NotifyDataSetChanged")
+//            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+//                if (response.isSuccessful) {
+//                    val charactersResponse = response.body()
+//                    characterAdapter.character.clear()
+//                    if (charactersResponse != null) {
+//                        val animeCharacters : JsonArray? =
+//                            charactersResponse.getAsJsonArray("characters")
+//                        if (animeCharacters != null) {
+//                            for (characters in 0 until animeCharacters.size()) {
+//                                val characterObject : JsonObject? =
+//                                    animeCharacters.get(characters) as JsonObject?
+//                                if (characterObject != null) {
+//                                    val character = Character()
+//
+//                                    character.mal_id =
+//                                        characterObject.get("mal_id").asInt
+//                                    character.image_url =
+//                                        characterObject.get("image_url").asString
+//                                    character.name =
+//                                        characterObject.get("name").asString
+//
+//                                    characterAdapter.character.add(character)
+//                                }
+//                            }
+//                            characterAdapter.notifyDataSetChanged()
+//                        }
+//                    }
+//                }
+//            }
+//        })
+    }
+
+    private fun loadMore() {
+        val malID = arguments?.getString("mal_id").toString()
+        val characterApi = JikanApiInstanceHelper.getJikanApiInstance().create(CharacterApi::class.java)
 
         characterApi.getCharactersStaff(malID).enqueue(object : Callback<JsonObject> {
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
@@ -88,4 +164,14 @@ class CharacterFragment : Fragment() {
             }
         })
     }
+
+//    private fun addScrollistener() {
+//        binding.animeCharacter.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+//                super.onScrollStateChanged(recyclerView, newState)
+////                val totalItemCount = recyclerView.layoutManager!!.itemCount
+//
+//            }
+//        })
+//    }
 }
