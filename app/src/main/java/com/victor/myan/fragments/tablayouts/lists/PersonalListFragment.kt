94a6e1010c3c23c5f83ccdb3d90520682183e9bd
MrVictor42 <guin-hu@hotmail.com>
2021-09-18 14:56:15 -0300

    Create fragment personal

--- app/src/main/java/com/victor/myan/fragments/tablayouts/lists/PersonalListFragment.kt
+++ app/src/main/java/com/victor/myan/fragments/tablayouts/lists/PersonalListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.victor.myan.R
 import com.victor.myan.databinding.FragmentPersonalListBinding
+import com.victor.myan.fragments.tablayouts.lists.crud.CreateListFragment
 
 class PersonalListFragment : Fragment() {
 
@@ -26,5 +29,20 @@ class PersonalListFragment : Fragment() {
     ): View {
         binding = FragmentPersonalListBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val btnAddPlaylist = binding.btnAddPlaylist
+
+        btnAddPlaylist.setOnClickListener {
+            val createListFragment = CreateListFragment()
+            (view.context as FragmentActivity)
+                .supportFragmentManager
+                .beginTransaction()
+                .remove(this)
+                .replace(R.id.fragment_layout, createListFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
