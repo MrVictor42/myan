ba4c28c3467077094a3028b3a9da5342cb216453
MrVictor42 <guin-hu@hotmail.com>
2021-09-18 23:25:45 -0300

    Create fragment to register list, and catch photo for cellphone

--- app/src/main/java/com/victor/myan/fragments/tablayouts/lists/crud/CreateListFragment.kt
+++ app/src/main/java/com/victor/myan/fragments/tablayouts/lists/crud/CreateListFragment.kt
@@ -1,15 +1,24 @@
 package com.victor.myan.fragments.tablayouts.lists.crud
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import com.victor.myan.R
 import com.victor.myan.databinding.FragmentCreateListBinding
 
 class CreateListFragment : Fragment() {
 
     private lateinit var binding : FragmentCreateListBinding
+    private lateinit var btnButtonImage : AppCompatButton
+    private var uri : Uri? = null
 
     companion object {
         fun newInstance(): CreateListFragment {
@@ -26,5 +35,39 @@ class CreateListFragment : Fragment() {
     ): View {
         binding = FragmentCreateListBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnButtonImage = binding.btnSelectImage
+        val btnButtonRegister = binding.btnRegister
+
+        btnButtonImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+        btnButtonRegister.setOnClickListener {
+            if(uri == null) {
+                uri = Uri.parse(
+                    ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                            + resources.getResourcePackageName(R.drawable.chapeu) + '/'
+                            + resources.getResourceTypeName((R.drawable.chapeu)) + '/'
+                            + resources.getResourceEntryName(R.drawable.chapeu)
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK && data != null) {
+            uri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, uri)
+            val circleImage = binding.circleImage
+            circleImage.setImageBitmap(bitmap)
+            btnButtonImage.alpha = 0f
+        }
     }
 }
